4faa032f4dece8b1012454ca598def663322c15c
Mikel Gonzalez <agcompguru@gmail.com>
2012-03-26 18:00:18 -0400

    Finished week one project

--- Objective C1/ObjectiveC1-Week1/ObjectiveC1-Week1/AppDelegate.m
+++ Objective C1/ObjectiveC1-Week1/ObjectiveC1-Week1/AppDelegate.m
@@ -14,6 +14,63 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //float int conversion
+    float floatCanons = 10;
+    int intCanons = (int)floatCanons;
+    
+    NSLog(@"The pirates had %f canon's or %d canon's if you want to be techinal.... Garrrhh", floatCanons, intCanons);
+    
+    //float int and bool requirement
+    float pirateOneHeight = 5.2;
+    float pirateTwoHeight = 6.4;
+    
+    int pirateOneEyes = 2;
+    int pirateTwoEyes = 1;
+    
+    BOOL pirateOneCanRead = YES;
+    BOOL pirateTwoCanRead = NO;
+    
+    //if else statement and the "and" "or" comparisons
+    if (pirateOneHeight > pirateTwoHeight || (pirateOneEyes > pirateTwoEyes && pirateOneCanRead)) {
+        NSLog(@"Pirate One is probably more likely to be able to head the canon operation because he can read and he has two eyes, the height is a disadvantage, but he will be less likely to get his eye blown out like pirate two if he can read the directions");
+    }else if(pirateTwoHeight > pirateOneHeight){
+        NSLog(@"Pirate two has the whole package. He is tall and can read and has two eyes. He should definately head the canon operations! Garrhh.");
+    }
+    
+    //for loop
+    NSLog(@"Which pirates will volunteer to help man the canons?");
+    for (int i = 3; i <= 10; i++) {
+        NSLog(@"Pirate %d Volunteers!", i);
+    }
+    
+    //Neste for loops;
+    NSLog(@"Do all of you pirates have all of your eyes?");
+    for (int i = 1; i <= 10; i++) {
+        for (int n = 1; n <= 2; n++) {
+            if (i == 2){
+                if (n == 1){
+                    NSLog(@"umm %d", n);
+                }else {
+                    NSLog(@"Ummm I'm pirate %d and... nope, just one", i);
+                }
+            }else{
+                NSLog(@"umm %d", n);
+                if (n == 2) {
+                    NSLog(@"Im pirate %d and I got %d eyes!!! GArhhhh", i, n);
+                }
+            }
+        }
+    }
+    
+    //wile loop
+    NSLog(@"OK well we will be sailing in 10 seconds so here's a break!");
+    int count = 1;
+    while (count <= 10) {
+        NSLog(@"%d", count);
+        count++;
+    }
+    NSLog(@"Garhh harharharharhahaha... There's your break! Now get to work!");
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
