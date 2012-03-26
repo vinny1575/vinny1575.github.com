e395516f6b1c68870aeec4dd86d3a8e6dc0788f6
Mikel Gonzalez <agcompguru@gmail.com>
2012-03-26 17:28:04 -0400

    Inintial Setup for week 1 project

---
+++ Objective C1/ObjectiveC1-Week1/ObjectiveC1-Week1Tests/ObjectiveC1_Week1Tests.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveC1_Week1Tests.h
+//  ObjectiveC1-Week1Tests
+//
+//  Created by Alan Gonzalez on 3/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ObjectiveC1_Week1Tests : SenTestCase
+
+@end
---
+++ Objective C1/ObjectiveC1-Week1/ObjectiveC1-Week1Tests/ObjectiveC1_Week1Tests.m
@@ -0,0 +1,32 @@
+//
+//  ObjectiveC1_Week1Tests.m
+//  ObjectiveC1-Week1Tests
+//
+//  Created by Alan Gonzalez on 3/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ObjectiveC1_Week1Tests.h"
+
+@implementation ObjectiveC1_Week1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ObjectiveC1-Week1Tests");
+}
+
+@end
