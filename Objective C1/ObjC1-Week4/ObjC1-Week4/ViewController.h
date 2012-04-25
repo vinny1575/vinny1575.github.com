da5aad8d0f0f34de1b03383aa0eccad44f037f2b
Mikel Gonzalez <agcompguru@gmail.com>
2012-04-25 16:17:08 -0400

    added button target and changed some colors

--- Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.h
+++ Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.h
@@ -10,4 +10,5 @@
 
 @interface ViewController : UIViewController
 
+- (void)onClick;
 @end
--- Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.m
+++ Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.m
@@ -16,19 +16,35 @@
 
 - (void)viewDidLoad
 {
+    
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    
     UILabel *userName = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 120, 40)];
     userName.text = @"Username:";
+    [userName setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:userName];
-    UITextField *userNameTxt = [[UITextField alloc] initWithFrame:CGRectMake(140, 10, 200, 40)];
+    UITextField *userNameTxt = [[UITextField alloc] initWithFrame:CGRectMake(120, 10, 200, 40)];
+    [userNameTxt setBorderStyle:UITextBorderStyleRoundedRect];
     [self.view addSubview:userNameTxt];
     UIButton *buttonLogin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [buttonLogin setFrame:CGRectMake(250, 60, 100, 40)];
+    [buttonLogin setFrame:CGRectMake(220, 60, 100, 40)];
     [buttonLogin setTitle:@"Login" forState:UIControlStateNormal];
+    [buttonLogin addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:buttonLogin];
+    
+    UILabel *pleaseEnter = [[UILabel alloc] initWithFrame:CGRectMake(0, 120, 320, 80)];
+    pleaseEnter.text = @"Please Enter Username";
+    [pleaseEnter setTextColor:[UIColor blueColor]];
+    [pleaseEnter setTextAlignment:UITextAlignmentCenter];
+    [pleaseEnter setBackgroundColor:[UIColor lightGrayColor]];
+    [self.view addSubview:pleaseEnter];
+    
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+-(void)onClick
 
 - (void)viewDidUnload
 {
