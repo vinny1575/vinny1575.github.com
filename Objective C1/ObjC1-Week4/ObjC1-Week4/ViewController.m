0e427625fc16f4d96d28f84bb280e6a7ff06e3c3
Mikel Gonzalez <agcompguru@gmail.com>
2012-04-25 16:09:45 -0400

    added label, textfield, and button

--- Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.m
+++ Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.m
@@ -16,6 +16,16 @@
 
 - (void)viewDidLoad
 {
+    UILabel *userName = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 120, 40)];
+    userName.text = @"Username:";
+    [self.view addSubview:userName];
+    UITextField *userNameTxt = [[UITextField alloc] initWithFrame:CGRectMake(140, 10, 200, 40)];
+    [self.view addSubview:userNameTxt];
+    UIButton *buttonLogin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [buttonLogin setFrame:CGRectMake(250, 60, 100, 40)];
+    [buttonLogin setTitle:@"Login" forState:UIControlStateNormal];
+    [self.view addSubview:buttonLogin];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
