645a5e1154ae1453aeffa6907de65a6cb76e4583
Mikel Gonzalez <agcompguru@gmail.com>
2012-04-25 16:05:24 -0400

    Starting Week 4 Project

---
+++ Objective C1/ObjC1-Week4/ObjC1-Week4/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ObjC1-Week4
+//
+//  Created by Alan Gonzalez on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Objective C1/ObjC1-Week4/ObjC1-Week4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ObjC1-Week4
+//
+//  Created by Alan Gonzalez on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
