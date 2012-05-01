1255ec95f18a7d36bafe976c3fee89e20ecae978
Mikel Gonzalez <agcompguru@gmail.com>
2012-05-01 15:03:35 -0400

    Shape Factory Created

---
+++ Objective C2/ObjC-Week1/ObjC-Week1/ShapeFactory.m
@@ -0,0 +1,13 @@
+//
+//  ShapeFactory.m
+//  ObjC-Week1
+//
+//  Created by Alan Gonzalez on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShapeFactory.h"
+
+@implementation ShapeFactory
+
+@end
