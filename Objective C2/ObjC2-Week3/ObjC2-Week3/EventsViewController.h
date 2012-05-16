b7f5ad2c0156fd43bb58dd6d43ccc9b53848bbcf
Mikel Gonzalez <agcompguru@gmail.com>
2012-05-15 23:05:45 -0400

    renamed first view controller

--- Objective C2/ObjC2-Week3/ObjC2-Week3/EventsViewController.h
+++ Objective C2/ObjC2-Week3/ObjC2-Week3/EventsViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface EventsViewController : UIViewController
 
 - (IBAction)addEvent:(id)sender;
 @property (strong, nonatomic) IBOutlet UITextView *eventsTxtView;
--- Objective C2/ObjC2-Week3/ObjC2-Week3/EventsViewController.m
+++ Objective C2/ObjC2-Week3/ObjC2-Week3/EventsViewController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "EventsViewController.h"
 #import "AddEventViewController.h"
 
-@interface ViewController () <EventDelegate>
+@interface EventsViewController () <EventDelegate>
 
 @end
 
-@implementation ViewController
+@implementation EventsViewController
 
 //delegate method
 @synthesize eventsTxtView;
