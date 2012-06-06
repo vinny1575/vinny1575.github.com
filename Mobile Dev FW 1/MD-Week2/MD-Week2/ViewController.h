19fb05daa1170e01d6fc7800c1e76b01558e563a
Mikel Gonzalez <agcompguru@gmail.com>
2012-06-06 13:19:12 -0400

    Added tableviewcontroller subcalss

---
+++ Mobile Dev FW 1/MD-Week2/MD-Week2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MD-Week1
+//
+//  Created by Alan Gonzalez on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>{
+    NSMutableArray *foodArray;
+}
+
+@end
---
+++ Mobile Dev FW 1/MD-Week2/MD-Week2/ViewController.m
@@ -0,0 +1,87 @@
+//
+//  ViewController.m
+//  MD-Week1
+//
+//  Created by Alan Gonzalez on 5/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Cell.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    //I decided to use a csv file so that I can change my list easily.
+    NSString *fileString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"food" ofType:@"csv"] encoding:NSUTF8StringEncoding error:nil];
+    foodArray = [NSMutableArray arrayWithArray:[fileString componentsSeparatedByString:@","]];
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return foodArray.count;
+}
+
+//setup each cell one by one
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //use the identifier I set.
+    static NSString *CellIdentifier = @"myCell";
+    //use custom class
+    Cell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[Cell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.imageView.image = [UIImage imageNamed:@"star"];
+    cell.title.text = [foodArray objectAtIndex:indexPath.row];
+    cell.subTitle.text = @"Subtitle for Cell";
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self performSegueWithIdentifier:@"trans" sender:self];
+}
+
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    //removes itm fromt eh array and reloads the table
+    [foodArray removeObjectAtIndex:indexPath.row];
+    [tableView reloadData];
+}
+
+@end
